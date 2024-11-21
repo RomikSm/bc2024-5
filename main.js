f35fe275c48d93ee0f876a387d7fc5155530d257
RomikSm b2024-5 <roma.semen.com@gmail.com>
2024-11-21 08:36:20 +0200

    get, put, delete

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const {program} = require('commander');
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
@@ -11,6 +13,42 @@ program
 program.parse();
 
 const options = program.opts();
+
+app.use(express.text());
+app.use(express.json());
+
+app.get('/notes/:noteName', (req, res) => {
+    const notePath = path.join(options.cache, `${req.params.noteName}.txt`);
+
+    if (!fs.existsSync(notePath)) {
+        return res.status(404).send('Note not found');
+    }
+
+    const noteContent = fs.readFileSync(notePath, 'utf8');
+    res.send(noteContent);
+});
+
+app.put('/notes/:noteName', (req, res) => {
+    const notePath = path.join(options.cache, `${req.params.noteName}.txt`);
+
+    if (!fs.existsSync(notePath)) {
+        return res.status(404).send('Note not found');
+    }
+
+    fs.writeFileSync(notePath, req.body);
+    res.status(200).send('Note updated successfully');
+
+});
+
+app.delete('/notes/:noteName', (req, res) => {
+    const notePath = path.join(options.cache, `${req.params.noteName}.txt`);
+
+    if (!fs.existsSync(notePath)){
+        return res.status(404).send('Note not found');
+    }
+    fs.unlinkSync(notePath);
+    res.status(200).send('Note deleted successfully');
+});
 
 app.listen(options.port, options.host, () => {
     console.log(`Server started on http://${options.host}:${options.port}`);
